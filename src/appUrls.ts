bd930b6620f394312b7ede5a53f0a2f871c4ef56
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2022-11-07 10:53:31 +0900

    deliciouShare.appを追加した #3

--- src/appUrls.ts
+++ src/appUrls.ts
@@ -6,6 +6,7 @@ const appUrls = [
   "https://yu-ko-ba.github.io/genkaikor/",
   "https://yu-ko-ba.github.io/repeater/",
   "https://yu-ko-ba.github.io/post-image-as-base64-example/",
+  "https://openbeta.delicioushare.app",
 ]
 
 export default appUrls
