ca94525a28459c2d12b937adf837210c307b32e1
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2023-07-17 19:01:47 +0900

    location-remover を追加しました。#12

--- src/appUrls.ts
+++ src/appUrls.ts
@@ -7,6 +7,7 @@ const appUrls = [
   "https://yu-ko-ba.github.io/repeater/",
   "https://yu-ko-ba.github.io/post-image-as-base64-example/",
   "https://ym-previewer.vercel.app",
+  "https://yu-ko-ba.github.io/location-remover/",
   "https://non-drawing-probability-calculator.vercel.app",
 ];
 
