554a6084bc238edbacfae0ba2026a2516a55b731
yu-ko-ba <yu-ko-ba@outlook.com>
2023-08-15 09:45:45 +0900

    PWA対応しました。

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
   return (
     <Html lang="ja">
       <Head>
+        <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;400;500;700&display=swap" rel="stylesheet" />
